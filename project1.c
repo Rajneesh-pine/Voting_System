8456d5e6473f118adbe5009c78e464e0f4294dd1
Rajneesh Jaiswal <145674922+Rajneesh-pine@users.noreply.github.com>
2024-09-27 05:38:42 +0530

    Update project1.c

--- project1.c
+++ project1.c
@@ -1,148 +1,148 @@
 #include <stdio.h>
-#include<string.h>
-int v[10];
+#include <string.h>
+
+int v[3];  // Voting counts for 3 parties
+
 void members()
 {
-    
-    printf("Here is the list of the participants \n ");
-    printf("1.BJP \n ");
-    printf("2.BSP \n ");
-    printf("3.congress \n ");
-    
+    printf("Here is the list of the participants: \n");
+    printf("1. BJP\n");
+    printf("2. BSP\n");
+    printf("3. Congress\n");
 }
-void vote(){
+
+void vote()
+{
     int d;
-      members();
-      printf("now choose any one  ");
-      scanf("%d",&d);
-      switch (d)
-      {
-      case 1:
-      {
-        v[0]++;
-        printf("you have sucessfully given your vote \n ");
-      }
-        /* code */
-        break;
-     case 2:
-     {
-        v[1]++;
-        printf("you have sucessfully given your vote \n ");
-     }
-     break;
-     case 3:
-     {
-        v[2]++; 
-        printf("you have sucessfully given your vote \n ");    }
-        break;
-      
-      default:printf("enter something valid \n ");
-        break;
-      }
-
-
+    members();
+    printf("Now choose any one: ");
+    scanf("%d", &d);
+    switch (d)
+    {
+        case 1:
+            v[0]++;
+            printf("You have successfully given your vote to BJP.\n");
+            break;
+        case 2:
+            v[1]++;
+            printf("You have successfully given your vote to BSP.\n");
+            break;
+        case 3:
+            v[2]++;
+            printf("You have successfully given your vote to Congress.\n");
+            break;
+        default:
+            printf("Please enter a valid option.\n");
+            break;
+    }
 }
 
 void voters()
 {
+    int h;
+    char n[50], c[12];
+    int continue_voting = 1;
 
-    int h;char n[50];char c[12];
-    printf("you have entered into the voters mode  \n ");
-    label:
-    printf("enter your name \n ");
-    scanf("%s",n);//%s is for string
-    printf("enter your aadhar card number \n  ");
-    scanf("%s",c);
-  if(((strcmp(n,"rajneesh")==0) && (strcmp(c,"rajneesh123")==0))  || 
-       ((strcmp(n,"harshit")==0) && (strcmp(c,"harshit1234")==0))||
-       ((strcmp(n,"dipanshu")==0) && (strcmp(c,"dipanshu123")==0)) ||
-       ((strcmp(n,"mudit")==0) && (strcmp(c,"mudit123456")==0)))
-    {
-        printf("you are eleigible to give vote \n ");
-         vote();
+    printf("You have entered the voter mode.\n");
 
-    }
-    else
+    while (continue_voting)
     {
-        printf("you are not eligible for voting \n");
+        printf("Enter your name: ");
+        scanf("%s", n);  // %s is for string
+
+        printf("Enter your Aadhar card number: ");
+        scanf("%s", c);
+
+        if (((strcmp(n, "rajneesh") == 0) && (strcmp(c, "rajneesh123") == 0)) ||
+            ((strcmp(n, "harshit") == 0) && (strcmp(c, "harshit1234") == 0)) ||
+            ((strcmp(n, "dipanshu") == 0) && (strcmp(c, "dipanshu123") == 0)) ||
+            ((strcmp(n, "mudit") == 0) && (strcmp(c, "mudit123456") == 0)))
+        {
+            printf("You are eligible to give a vote.\n");
+            vote();
+        }
+        else
+        {
+            printf("You are not eligible for voting.\n");
+        }
+
+        printf("Press 1 to continue voting or 0 to exit: ");
+        scanf("%d", &h);
+        if (h != 1)
+        {
+            continue_voting = 0;
+        }
     }
-    printf("press 1 to continue this mode \t ");
-    scanf("%d",&h);if((h==1))
-    {goto label;}
-    else
-    {
-        main();
-    }
-    
-   
-    
 }
-
-/// @brief 
-
-/// @brief 
 
 void result()
 {
-    
-    int max=v[0];int m=0;
-    for(int i=0;i<sizeof(v)/sizeof(v[0]);i++)
-    {
-        if(v[i]>=max)
-        {
-            max=v[i];
-            m=i;
-        }
-    
-    }
-   
-    printf("The name of  party \t  \t no of votes gained \n");
-    printf("\t 1.BJP \t  \t  = \t %d \n ",v[0]);
-    printf("\t 2.BSP \t  \t  = \t %d \n ",v[1]);
-    printf("\t 3.congress\t  = \t %d \n ",v[2]);
-     switch (m)
-    {
-    case /* constant-expression */0:
-          printf("\n \n winner is BJP with max no of votes \n ");
+    int max = v[0], m = 0;
 
-        /* code */
-        break;
-        case 1: printf("\n \n winner is BSP with max no of votes \n  ");
-        break;
-        case  2:printf("\n \n winner is congress with max no votes \n ");
-        break;
-    
-    default:
-        printf("no one won");
-        break;
+    // Determine the party with the maximum votes
+    for (int i = 1; i < 3; i++)
+    {
+        if (v[i] > max)
+        {
+            max = v[i];
+            m = i;
+        }
+    }
+
+    // Display the results
+    printf("The name of the party \t  \t no of votes gained\n");
+    printf("\t 1. BJP \t  = \t %d \n", v[0]);
+    printf("\t 2. BSP \t  = \t %d \n", v[1]);
+    printf("\t 3. Congress \t  = \t %d \n", v[2]);
+
+    // Display the winner
+    switch (m)
+    {
+        case 0:
+            printf("\nWinner is BJP with the maximum number of votes!\n");
+            break;
+        case 1:
+            printf("\nWinner is BSP with the maximum number of votes!\n");
+            break;
+        case 2:
+            printf("\nWinner is Congress with the maximum number of votes!\n");
+            break;
+        default:
+            printf("No valid winner.\n");
+            break;
     }
 }
-    
+
 void main()
 {
-    int b=0;
-    printf("Welcome to the Voting System \n ");
-    label2:
-    printf("for entering into the voter mode enter 1 \n ");
-    printf("for entering into results enter 2 \n ");
-    printf("press 3 for exit \n ");
+    int b = 0;
+    int exit_program = 0;
 
-    scanf("%d",&b);
-    switch (b)
+    printf("Welcome to the Voting System!\n");
+
+    while (!exit_program)
     {
-    case /* constant-expression */1:
-           voters();
-        /* code */
-        break;
-    case 2:
-        result();
-        break;
-        case 3:
-           printf("\n\tThank you visiting our site. \n ");
-        break;
-    
-    default:printf("enter something valid \n "); goto label2;
-    }
+        printf("\n1. Enter voter mode\n");
+        printf("2. View results\n");
+        printf("3. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &b);
 
-    
+        switch (b)
+        {
+            case 1:
+                voters();
+                break;
+            case 2:
+                result();
+                break;
+            case 3:
+                printf("\nThank you for visiting our site.\n");
+                exit_program = 1;
+                break;
+            default:
+                printf("Please enter a valid option.\n");
+                break;
+        }
+    }
 }
